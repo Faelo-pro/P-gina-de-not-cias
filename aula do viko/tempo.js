879dcc654638ad00db0445c034d93fa99dc0a53e
Faelo <102743508+Faelo-pro@users.noreply.github.com>
2022-04-20 10:41:58 -0300

    file:///C:/Users/fritz/OneDrive/Documents/aula%20do%20viko/index1.html

---
+++ aula do viko/tempo.js
@@ -0,0 +1,13 @@
+conte = document.getElementById("cont_fd469063455c97cb408684cf590d61d9");
+  if (conte) {
+      conte.style.cssText = "width: 272px; color: #993300; background-color: transparent; border:1px solid transparent; margin: 0 auto; font-family: Arial;";
+      elem = document.createElement("iframe");
+      elem.style.cssText = "width:272px; color:#993300; height:86px; background-color: transparent";
+      elem.id = "fd469063455c97cb408684cf590d61d9";
+      elem.src = "https://www.tempo.com/getwid/fd469063455c97cb408684cf590d61d9";
+      elem.frameBorder = 0;
+      elem.allowTransparency = true;
+      elem.scrolling = "no";
+      elem.name = "flipe";
+      conte.appendChild(elem);
+}
